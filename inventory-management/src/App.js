601ea7dd8323d4427231c7832bbd1b7e7f47ef1e
jtbyrnes-wpi <88510085+jtbyrnes-wpi@users.noreply.github.com>
2024-07-12 21:36:09 -0400

    Initialized single page application

---
+++ inventory-management/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Inventory Count
+        </p>
+        <button> Buy Now </button>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
